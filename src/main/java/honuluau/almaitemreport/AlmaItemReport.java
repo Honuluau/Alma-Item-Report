b71f4186053815024fa96d45b84f7921f73f6f1d
honuluau <honu.dylan@gmail.com>
2024-11-18 18:21:43 -0500

    Finally done fixing maven, java runtime environment, and java development kits to work.

--- src/main/java/honuluau/almaitemreport/AlmaItemReport.java
+++ src/main/java/honuluau/almaitemreport/AlmaItemReport.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package honuluau.almaitemreport;
 
-/**
- *
- * @author dd18787
- */
 public class AlmaItemReport {
 
     public static void main(String[] args) {
