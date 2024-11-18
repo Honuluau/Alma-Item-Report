c0b2f4eaf6606009571e5e6e4e7e4d772e91ada6
honuluau <honu.dylan@gmail.com>
2024-11-18 16:11:27 -0500

    Init

---
+++ src/main/java/honuluau/almaitemreport/AlmaItemReport.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package honuluau.almaitemreport;
+
+/**
+ *
+ * @author dd18787
+ */
+public class AlmaItemReport {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
